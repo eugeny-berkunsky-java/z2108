91a5b74dd164350bed740fa1922fed402e248ea1
kafedra-iust <kafedra.iust@gmail.com>
2020-08-21 18:32:39 +0300

    Initial commit

---
+++ src/ua/mk/berkut/te/Main.java
@@ -0,0 +1,15 @@
+package ua.mk.berkut.te;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    new Main().run();
+    }
+
+    private void run() {
+
+    }
+
+}
